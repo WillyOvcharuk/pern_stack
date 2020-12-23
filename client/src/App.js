b09120b41033970785aeca9259845107c2d2be48
test <willyovcharuk1994@gmail.com>
2020-12-23 23:54:35 +0200

    create inputTodo fragment

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import  React, { Fragment } from 'react'
+
+
+//components
+import InputTodo from './components/InputTodos'
+function App() {
+  return (
+    <Fragment>
+      <div className="container">
+        <InputTodo />
+      </div>
+    </Fragment>
+  );
+}
+
+export default App;
